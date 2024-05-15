3e0370f0126699bcfbc113548650d5277248e95b
Sam Dravitzki <samdravitzki@gmail.com>
2024-05-15 19:09:44 +1200

    Added base to vite config to get build to work with github pages

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite'
+
+
+export default defineConfig({
+    base: 'samdravitzki' //  Required for github pages https://vitejs.dev/guide/static-deploy#github-pages
+})
