defcb51c24d8abba7ed67187c43545bf08f4aab7
Sam Dravitzki <samdravitzki@gmail.com>
2024-05-21 22:31:06 +1200

    Added min argument to the randomInt function

--- src/lib/randomInt/randomInt.ts
+++ src/lib/randomInt/randomInt.ts
@@ -1,3 +1,21 @@
-export default function randomInt(max: number, interval: number): number {
-    return Math.ceil(Math.floor(Math.random() * max) / interval) * interval;
+/**
+ * Generate a random integer between a minimum and maximum
+ * 
+ * Based on https://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range
+ * 
+ * @param max the maximum integer that can be generated
+ * @param min the minimum integer that can be generated
+ * @param interval only random numbers will be generated that are multiples of of the interval
+ * @returns 
+ */
+export default function randomInt(
+    max: number,
+    min: number = 0,
+    interval: number = 1
+): number {
+    if (min > max) {
+        throw new Error('min cannot be larger than max')
+    }
+
+    return Math.ceil(Math.floor((Math.random() * (max - min)) + min) / interval) * interval;
 }
