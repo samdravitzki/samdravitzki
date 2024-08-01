a374d5720dfdcc3e5ff9ccc7d7c666f5cae5b7cf
Sam Dravitzki <samdravitzki@gmail.com>
2024-08-01 23:02:22 +1200

    Move all the custom components into thier own file

---
+++ src/pong/components.ts
@@ -0,0 +1,81 @@
+import Component from '../ecs/Component/Component';
+import Vector from '../Vector/Vector';
+
+
+export type Position = Component & {
+    name: 'position',
+    position: Vector,
+};
+
+export type Velocity = Component & {
+    name: 'velocity',
+    velocity: Vector,
+};
+
+export type PrimitiveShape = Component & {
+    name: 'primitive',
+    stroke: number[],
+    strokeWeight: number,
+    fill: false | number[],
+    dash?: number[],
+} & ({
+    type: 'circle',
+    radius: number,
+} | {
+    type: 'line',
+    start: Vector,
+    end: Vector,
+} | {
+    type: 'square',
+    width: number,
+    height: number,
+} | {
+    type: 'text',
+    align: 'left' | 'right',
+    size: number,
+    text: string,
+});
+
+export type Collider = Component & {
+    name: 'collider',
+}  & ({
+    type: 'aabb', // Axis-aligned bounding box (Just a rectangle that doesnt rotate)
+    width: number,
+    height: number,
+});
+
+export type BallComponent = Component & {
+    name: 'ball',
+}
+
+export type PaddleComponent = Component & {
+    name: 'paddle',
+}
+
+export type PlayerComponent = Component & {
+    name: 'player',
+}
+
+export type AiComponent = Component & {
+    name: 'ai',
+}
+
+export type BackboardComponent = Component & {
+    name: 'backboard',
+    owner: 'player' | 'ai',
+}
+
+export type ScoreComponent = Component & {
+    name: 'score',
+    value: number,
+}
+
+export type Collision = Component & {
+    name: 'collision',
+    // The collision point of the entity in local space
+    contactPoint: Vector,
+    // The normal of the point collided with
+    normal: Vector,
+    // The depth in which the collision occured
+    penetration: number,
+}
