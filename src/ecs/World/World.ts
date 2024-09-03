3aeb1b13447fd3a21631eb42818d1c07a9155af5
Sam Dravitzki <samdravitzki@gmail.com>
2024-09-03 22:45:11 +1200

    Import entity

--- src/ecs/World/World.ts
+++ src/ecs/World/World.ts
@@ -1,40 +1,6 @@
 import Bundle from '../Bundle/Bundle';
 import Component from '../Component/Component';
-
-type EntityId = string;
-
-type ComponentId = string;
-
-class Entity {
-    readonly id: EntityId = window.crypto.randomUUID();
-    private _components = new Map<ComponentId, Component>();
-
-    get components() {
-        return Array.from(this._components.values());
-    }
-
-    getComponent(componentName: string) {
-        return this._components.get(componentName);
-    }
-
-    addComponent(component: Component) {
-        const result = this._components.get(component.name);
-
-        if (result !== undefined) {
-            throw new Error(`Entity cannot have more than one component of type '${component.name}'`)
-        }
-        
-        this._components.set(component.name, component);
-    }
-
-    replaceComponent(component: Component) {
-        this._components.set(component.name, component);
-    }
-
-    removeComponent(componentName: string) {
-        this._components.delete(componentName);
-    }
-}
+import Entity, { EntityId } from '../Entity/Entity';
 
 
 /**
