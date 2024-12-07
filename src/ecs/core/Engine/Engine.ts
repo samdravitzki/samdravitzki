6ee29d325cc94b5acc18382fbb91c3a68817f54a
samdravitzki <46504201+samdravitzki@users.noreply.github.com>
2024-12-07 05:13:27 +0000

    Reduced comments in the engine file by moving out to a notes markdown file

--- src/ecs/core/Engine/Engine.ts
+++ src/ecs/core/Engine/Engine.ts
@@ -6,54 +6,6 @@ import Vector from "../../../Vector/Vector";
 import State from "../State/State";
 
 /**
- * Resources:
- *
- * Global state shared between systems that is not associated with any
- * entity in particular
- *
- * Resources add the flexibility to break out of the ecs pattern adding
- * the ability to implement solutions in different ways that may be more
- * appropiate to solve the problem at hand
- *
- * Examples of this type of state could be score or the current key pressed
- *
- * Reference
- * - https://bevy-cheatbook.github.io/programming/res.html
- * - https://www.gamedev.net/forums/topic/710271-where-should-shared-resources-live-in-an-ecs-engine/
- */
-// type Resource = {
-
-// }
-
-/**
- * Conditional Systems:
- *
- * Implementations of ECS all seem to eventually need a way to orchestrate in what situations
- * systems will run. Both Unity and Bevy implement a mechanism for doing this with Unitys being
- * Component System Groups and Bevys being States
- *
- * Reference
- * - https://docs.unity3d.com/Packages/com.unity.entities@1.0/manual/systems-update-order.html
- * - https://github.com/bevyengine/bevy/blob/main/examples/games/game_menu.rs
- *
- * Currently I am running into an issue where I want the game to have a pause menu and a main menu,
- * but in each menu you want different systems to run and others to stop. It looks like I will need
- * to introduce a similiar concept to orchestrate functions into this codebase to introduce menus.
- * The goal is I should end up with something that can stop the game systems when opening the pause
- * menu, trigger some systems to run to setup the menu and run systems while the menu is open. Then
- * when the menu is closed a set of systems runs to close the menu and unpause. This should also
- * be generalised so that it can be applied to other scenarios in which I want to conditionally run
- * systems
- *
- * These states could be modeled in a simlar way to how they are in react?
- *
- * An alternative approach to conditional systems is to have multiple worlds and conditions for determining
- * which world is available to the user. The issue for me with this tho is it is almost exactly the same
- * as doing conditional systems except for if you can't have the same systems that run no matter whether
- * the game is paused or not meaning its just seems less flexible
- */
-
-/**
  * Testing out this way of implementing the builder pattern in typescript
  * so that as you run each build command it also incrementally builds the
  * types.
