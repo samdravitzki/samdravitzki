d8f4adfe088202f28cd8b874076483a9de984d9f
samdravitzki <46504201+samdravitzki@users.noreply.github.com>
2025-03-06 10:00:45 +0000

    play through house beat sequence as user types

--- src/drums/drums.ts
+++ src/drums/drums.ts
@@ -6,8 +6,9 @@ import primitiveRenderer from "../ecs/parts/primitive-renderer/primitive-rendere
 import randomInt from "../lib/randomInt/randomInt";
 import { PrimitiveShape } from "../ecs/parts/primitive-renderer/components/Primitive";
 import Component from "../ecs/core/Component/Component";
+import Bounds from "../ecs/core/Bounds/Bounds";
 
-const drums = EngineBuilder.create().build();
+const drums = EngineBuilder.create().state("sequence-index", 0).build();
 
 /**
  * Used Drumhaus (https://github.com/mxfng/drumhaus/tree/main) an in browser
@@ -16,42 +17,118 @@ const drums = EngineBuilder.create().build();
 
 drums.part(primitiveRenderer);
 
+const kick = new Tone.Sampler({
+  urls: {
+    ["C2"]: "./samples/drumhaus-paris/paris_kick.wav",
+  },
+}).toDestination();
+
+// prettier-ignore
+// const sequences = [ // raw
+//   [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0],
+//   [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+//   [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0],
+//   [0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0]
+// ];
+
+// prettier-ignore
+const sequences = [ // edited
+  [1, 0, 1, 0, 1, 0, 1, 0],
+  [0, 0, 1, 0, 0, 0, 1, 0],
+  [1, 0, 1, 0, 1, 0, 1, 0],
+  [0, 1, 0, 1, 0, 1, 0, 1]
+];
+
+const kickSequence = sequences[0];
+
 const clap = new Tone.Sampler({
   urls: {
     ["C2"]: "./samples/drumhaus-paris/paris_clap.wav",
   },
 }).toDestination();
 
-drums.system("drum", { event: "keypress" }, (_world, { p }) => {
-  clap.triggerAttackRelease("C2", "1n");
-});
+const clapSequence = sequences[1];
+
+const hat = new Tone.Sampler({
+  urls: {
+    ["C2"]: "./samples/drumhaus-paris/paris_hat.wav",
+  },
+}).toDestination();
+
+const hatSequence = sequences[2];
+
+const openHat = new Tone.Sampler({
+  urls: {
+    ["C2"]: "./samples/drumhaus-paris/paris_ohat.wav",
+  },
+}).toDestination();
+
+const openHatSequence = sequences[3];
+
+function randomlyPositionedTextBundle(emoji: string, canvasBounds: Bounds) {
+  const position = Vector.create(
+    randomInt(canvasBounds.max.x - canvasBounds.min.x),
+    randomInt(canvasBounds.max.y - canvasBounds.min.y)
+  );
+
+  const emojiBundle = createBundle([
+    "emoji",
+    {
+      name: "position",
+      position,
+    },
+    {
+      name: "primitive",
+      fill: [240, 60, 100, 255],
+      type: "text",
+      text: emoji,
+      align: "left",
+      size: 25,
+    },
+  ]);
+
+  return emojiBundle;
+}
 
 drums.system(
-  "emoji-spawner",
+  "drum",
   { event: "keypress" },
-  (world, { canvasBounds }) => {
-    const position = Vector.create(
-      randomInt(canvasBounds.max.x - canvasBounds.min.x),
-      randomInt(canvasBounds.max.y - canvasBounds.min.y)
-    );
+  (world, { canvasBounds }, state) => {
+    const sequenceIndex = state["sequence-index"].value;
 
-    const clapEmoji = createBundle([
-      "emoji",
-      {
-        name: "position",
-        position,
-      },
-      {
-        name: "primitive",
-        fill: [240, 60, 100, 255],
-        type: "text",
-        text: "👏",
-        align: "left",
-        size: 25,
-      },
-    ]);
+    const time = Tone.now();
 
-    world.addBundle(clapEmoji);
+    if (clapSequence[sequenceIndex] === 1) {
+      clap.triggerAttackRelease("C2", "1n", time);
+      const clapEmoji = randomlyPositionedTextBundle("clap", canvasBounds);
+      world.addBundle(clapEmoji);
+    }
+
+    if (kickSequence[sequenceIndex] === 1) {
+      kick.triggerAttackRelease("C2", "1n", time);
+      const kickEmoji = randomlyPositionedTextBundle("kick", canvasBounds);
+      world.addBundle(kickEmoji);
+    }
+
+    if (hatSequence[sequenceIndex] === 1) {
+      hat.triggerAttackRelease("C2", "1n", time);
+      const hatEmoji = randomlyPositionedTextBundle("hat", canvasBounds);
+      world.addBundle(hatEmoji);
+    }
+
+    if (openHatSequence[sequenceIndex] === 1) {
+      openHat.triggerAttackRelease("C2", "1n", time);
+      const openHatEmoji = randomlyPositionedTextBundle(
+        "open hat",
+        canvasBounds
+      );
+      world.addBundle(openHatEmoji);
+    }
+
+    const nextIndex = (sequenceIndex + 1) % 8;
+
+    console.log(nextIndex);
+    state["sequence-index"].setValue(nextIndex);
   }
 );
 
