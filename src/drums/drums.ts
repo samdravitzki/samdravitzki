044a7ee66dbdac7843ac878f501b58314131c23e
samdravitzki <46504201+samdravitzki@users.noreply.github.com>
2025-03-19 09:47:39 +0000

    Added a bpm counter

--- src/drums/drums.ts
+++ src/drums/drums.ts
@@ -8,9 +8,14 @@ import { PrimitiveShape } from "../ecs/parts/primitive-renderer/components/Primi
 import Component from "../ecs/core/Component/Component";
 import Bounds from "../ecs/core/Bounds/Bounds";
 
+type Keypress = {
+  key: string;
+  time: number;
+};
+
 const drums = EngineBuilder.create()
   .state("sequence-index", 0)
-  .state<"key-presses", string[]>("key-presses", [])
+  .state<"key-presses", Keypress[]>("key-presses", [])
   .state<"active-sequence", string | null>("active-sequence", null)
   .build();
 
@@ -66,11 +71,66 @@ drums.system(
   }
 );
 
-const kick = new Tone.Sampler({
-  urls: {
-    ["C2"]: "./samples/drumhaus-paris/paris_kick.wav",
-  },
-}).toDestination();
+drums.system("bpm-text", { event: "start" }, (world, { canvasBounds }) => {
+  const textBundle = createBundle([
+    "bpm-text",
+    {
+      name: "position",
+      position: canvasBounds.top.left.plus(Vector.create(10, 30)),
+    },
+    {
+      name: "primitive",
+      fill: [240, 60, 100, 255],
+      type: "text",
+      text: "0",
+      align: "left",
+      size: 25,
+    },
+  ]);
+
+  world.addBundle(textBundle);
+});
+
+/**
+ * Given an array of beats work out the bpm they were played at
+ * @param beats an array of epoch timestamps relating to when each beat was played
+ */
+function deriveBpm(beats: number[]) {
+  if (beats.length < 2) {
+    return 0;
+  }
+
+  const timeDifferences = beats.slice(1).map((num, i) => num - beats[i]);
+
+  const total = timeDifferences.reduce(
+    (sum, difference) => sum + difference,
+    0
+  );
+  const msPerBeat = total / timeDifferences.length;
+  const bpm = 60_000 / msPerBeat;
+
+  return bpm;
+}
+
+/**
+ * Calculate the bpm as the user taps it out
+ *
+ * Based on https://www.all8.com/tools/bpm.htm
+ */
+drums.system("bpm-counter", { event: "update" }, (world, {}, state) => {
+  const keyPresses = state["key-presses"];
+  // The difference between each consecutive time
+  const bpm = deriveBpm(keyPresses.value.map((press) => press.time));
+
+  const [bpmText] = world.query<[PrimitiveShape]>(["primitive", "bpm-text"])[0];
+
+  if (bpmText.type === "text") {
+    // Each press atm triggers two beats atm (all the second beats triggered by a keypress is empty atm)
+    const halfBpm = Math.round(bpm / 2);
+
+    bpmText.text = `${halfBpm.toString()} bpm`;
+  }
+});
 
 // prettier-ignore
 const houseSequences = [
@@ -89,6 +149,12 @@ const hiphopSequences = [
   [0, 0, 0, 0, 0, 0, 0, 0], // open hat
   [0, 0, 1, 0, 0, 0, 1, 0], // snare
 ];
+
+const kick = new Tone.Sampler({
+  urls: {
+    ["C2"]: "./samples/drumhaus-paris/paris_kick.wav",
+  },
+}).toDestination();
 
 const clap = new Tone.Sampler({
   urls: {
@@ -138,10 +204,13 @@ drums.system(
     const activeSequence = state["active-sequence"];
     const sequenceIndex = state["sequence-index"];
 
-    keyPresses.push(p.key);
+    keyPresses.push({
+      key: p.key,
+      time: Date.now(),
+    });
 
     if (keyPresses.length >= 4) {
-      const [d, c, b, a] = keyPresses.slice(-4);
+      const [d, c, b, a] = keyPresses.map((press) => press.key).slice(-4);
 
       let patternDectected: string | null = null;
 
@@ -173,7 +242,7 @@ drums.system(
 
     // only keep sliding window of 4 most recent key presses
     const recentPresses = keyPresses.slice(-4);
-    console.debug(recentPresses);
+    console.debug(recentPresses.map((press) => press.key));
     state["key-presses"].setValue(recentPresses);
   }
 );
@@ -198,7 +267,7 @@ function randomlyPositionedTextBundle(text: string, canvasBounds: Bounds) {
   );
 
   const textBundle = createBundle([
-    "text",
+    "faded",
     {
       name: "position",
       position,
@@ -286,7 +355,7 @@ export type TextEffectComponent = Component & {
 drums.system("text-fade", { event: "update" }, (world, { p }) => {
   for (const [primitive, entityId] of world.query<
     [PrimitiveShape, string, TextEffectComponent]
-  >(["primitive", "entity-id", "text"])) {
+  >(["primitive", "entity-id", "faded"])) {
     if (primitive.fill) {
       const [r, g, b, alpha] = primitive.fill;
 
