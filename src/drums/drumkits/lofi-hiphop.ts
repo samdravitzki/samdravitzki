fdacb294634c321a158f7c5c4bb4caff2d7ad3ff
samdravitzki <46504201+samdravitzki@users.noreply.github.com>
2025-03-26 09:46:35 +0000

    Updated beat

--- src/drums/drumkits/lofi-hiphop.ts
+++ src/drums/drumkits/lofi-hiphop.ts
@@ -5,19 +5,19 @@ export const kick = new Tone.Sampler({
   urls: {
     ["C2"]: "./samples/itslucid-lofi/kick.wav",
   },
-}).toDestination();
+}).chain(new Tone.Panner(0), Tone.getDestination());
 
 export const hat = new Tone.Sampler({
   urls: {
     ["C2"]: "./samples/itslucid-lofi/hat.wav",
   },
-}).toDestination();
+}).chain(new Tone.Gain(0.7), new Tone.Panner(0), Tone.getDestination());
 
 export const snare = new Tone.Sampler({
   urls: {
     ["C2"]: "./samples/itslucid-lofi/snare.wav",
   },
-}).toDestination();
+}).chain(new Tone.Panner(0), Tone.getDestination());
 
 export const lofiHipHopDrumkit: Drumkit = {
   name: "hiphop", // Should be called "lofi-hiphop"
